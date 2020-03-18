eed02a10c735795961563791bbd8fb9d847e7523
ExistenceisP4ain <57858720+ExistenceisP4ain@users.noreply.github.com>
2020-03-18 20:17:25 +0100

    Delete routes.js

--- routes.js
+++
@@ -1,3 +0,0 @@
-let home = (req, res) => res.send('Hello World, ovaj puta iz paketa!'))
-let student = (req, res) => res.send('Ruta za studente preko novog paketa.'))
-export default { home, student }
