aa88e9e6594f738d17f1bfd4a39086228c2ea326
ExistenceisP4ain <57858720+ExistenceisP4ain@users.noreply.github.com>
2020-03-30 20:57:52 +0200

    Update Posts.vue

--- frontend/src/views/Posts.vue
+++ frontend/src/views/Posts.vue
@@ -1,75 +1,82 @@
 <template>
   <div v-if="store.authenticated">
-    <h1>Nema upisa u search nego je sve u created()</h1>
     <button @click="newImage()" type="Novi post" class="btn btn-primary ml-2">Post new image</button>
-    <div @click="gotoDetails(card)" :key="card.id" v-for="card in cards">
-      <InstagramCard :info="card"/>
+    <div @click="gotoDetails(post)" :key="post.id" v-for="post in posts">
+      <InstagramCard :info="post"/>
     </div>
   </div>
 </template>
 
 <script>
-import _ from 'lodash'
 import InstagramCard from "@/components/InstagramCard.vue";
 import store from "@/store.js";
+import _ from 'lodash'
 
 export default {
   data() {
-    return {
+    return{
       store,
-      cards: [],
+      posts:[],
+      term:''
     }
   },
-  watch: {
-    "store.searchTerm": _.debounce(function(val) {this.fetchPosts(val)}, 500)
-    //"store.searchTerm": function(val) {this.fetchPosts(val)}
-  },
-  created() {
-    this.search('iva','rijeka')
-    //this.search('nikola,null')
-    //this.search(null,'kopno')
-    //this.search()
-  },
+  
   name: "posts",
+  mounted(){
+    this.search();
+  },
   methods: {
-    fetchPosts(term, poCemu) {
-      term = term || store.searchTerm
-      fetch(`http://localhost:3000/posts?${poCemu}=${term}`)
-        .then(response => {
-          return response.json()
-        })
-        .then(data => {
-          console.log("Podaci s backenda", data)
-          this.cards = data.map(doc => {
-            return {id: doc.id, url: doc.source, email: doc.createdBy, title: doc.title, posted_at: Number(doc.postedAt)}
-          })
-          console.log(this.cards);
-        })
-    },
-    search(title, createdBy){
-      if(!title){
-        this.fetchPosts(createdBy,"createdBy")
-      }
-      else if(!createdBy){
-        this.fetchPosts(title, "title")
-      }
-      else if(!title && !createdBy){
-      }
-      else{
-        let term = title + " " +createdBy
-        this.fetchPosts(term, '_any')
-      }
+    fetchPosts(){
+    fetch(`http://localhost:3000/posts?_any=${this.store.searchTerm}`)
+    .then(data=>{
+      return data.json()
+    })
+    .then(sjz=>{
+      this.posts=sjz.map(doc=>{
+        return{
+          id:doc.id,
+          email:doc.createdBy,
+          postedat:Number(doc.postedAt),
+          url:doc.source,
+          title:doc.title
+        }
+      })
+    })
     },
     gotoDetails(card) {
       this.$router.push({path: `post/${card.id}`})
     },
     newImage() {
       this.$router.push({name: 'newpost'}).catch(err => console.log(err))
+    },
+    search(title, createdBy){
+	
+      title=this.store.searchTerm;
+      createdBy=this.store.searchTerm;
+	  
+      if(!title){
+        this.fetchPosts(createdBy)
+      }
+      else if(!createdBy){
+        this.fetchPosts(title)
+      }
+      else if (title && createdBy){
+        let nonjo=title+' '+createdBy
+        this.fetchPosts(nonjo)
+      }
+      else{
+        return  
+      }
     }
   },
   components: {
     InstagramCard
   },
+  watch:{
+    "store.searchTerm": _.debounce(function(){
+        this.search();
+    },500)
+  }
 }
 </script>
 
