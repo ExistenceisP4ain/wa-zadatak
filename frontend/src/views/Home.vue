6416d7b5032624d420d1d247ad268500a7d67817
ExistenceisP4ain <57858720+ExistenceisP4ain@users.noreply.github.com>
2020-04-01 22:10:31 +0200

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,6 +7,7 @@
       <router-view :term="term"/>
     </div>
     <div class="col-4">
+	
       <span v-if="authenticated">
         Current account: {{ userEmail }}
         <br/>
@@ -16,7 +17,25 @@
         <button type="Novi post" class="btn btn-primary btn-block d-none d-md-block">Post new image</button>
       </router-link>
     </div>
+	
+	 <table class="container" style="">
+	  <tr><th>BTC</th><th>Euro</th><th>USD</th></tr>
+	  <tr> <td>1</td>  <td>{{btceur}} €</td><td>{{btcusd}} $</td></tr>
+	  </table>
+<table style="">
+	  <tr><th>HRK</th><th>EUR</th></tr>
+	  <tr> <td>1 HRK</td>  <td>{{kuna}} €</td></tr>
+	  </table>	
+	  <table style="margin-left: 20px;">
+	  <tr><th>BTC</th><th>HRK</th></tr>
+	  <tr> <td>1 </td>  <td>{{kuna*btceur}} HRK</td></tr>
+	  </table>
+	  
+	  
+	  <br><br><br><br>
+	 
   </div>
+  
 </template>
 
 <script>
@@ -25,8 +44,47 @@ import store from "@/store.js";
 export default {
   props: ["term"],
   data() {
-    return store;
+    return {
+	store,
+	kuna:'',
+	btcusd:'',
+	btceur:''
+	}
   },
-  name: "home"
+  name: "home",
+  
+  mounted(){
+    fetch("https://api.exchangeratesapi.io/latest?symbols=HRK")
+    .then((response)=>{
+      return response.json()
+    })
+    .then((json)=>{
+      console.log("[1 € ] = ", json.rates.HRK, " HRK")
+      this.kuna=Number(json.rates.HRK)
+    })
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then((response)=>{
+      return response.json()
+    })
+    .then((json)=>{
+      console.log("Cijena BTC $:  ", json.bpi.USD.rate)
+      this.btcusd=json.bpi.USD.rate;
+		console.log("Cijena BTC €:  ", json.bpi.EUR.rate)
+      this.btceur=json.bpi.EUR.rate_float; // ne dela bez float za mnozenje *saduwu*	  
+    })
+}
 };
 </script>
+
+<style>
+
+table {
+  border-collapse: collapse;
+}
+
+table, th, td {
+  border: 1px solid black;
+  padding: 20px;
+}
+
+</style>
